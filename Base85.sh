7dcc349f2105a396a7182f4c0db1855cdca11d8f
Agade09 <n.sillitoe@gmail.com>
2019-07-17 21:20:46 +0000

    Modified example bash script to comment on which compiler was used to compile the source code

--- Base85.sh
+++ Base85.sh
@@ -2,6 +2,7 @@ if [ $# -lt 1 ]; then
     echo "$0: Takes at least one argument, the name of the AI to compile"
     exit 1
 fi
+Compiler=g++-9
 AI_Name=$1 #Get the AI name you passed as a command line parameter
 AI_Name="${AI_Name%.*}" #Remove extension in case you passed "V4.cpp" instead of "V4"
 if [ -f "$AI_Name".cpp ]; then #If the file exists
@@ -10,7 +11,7 @@ if [ -f "$AI_Name".cpp ]; then #If the file exists
 	sed -i '/^#pragma/d' "$AI_Name"_nopragma.cpp
 
 	#Compile your AI with whatever options you like
-	g++-9 "$AI_Name"_nopragma.cpp -o "$AI_Name" -Ofast -march=native -std=c++17 -w -s -flto -fno-rtti -fno-math-errno -ffunction-sections -fdata-sections -Wl,--gc-sections -Wl,-z,norelro -Wl,--hash-style=gnu #-fno-exceptions
+	"$Compiler" "$AI_Name"_nopragma.cpp -o "$AI_Name" -Ofast -march=native -std=c++17 -w -s -flto -fno-rtti -fno-math-errno -ffunction-sections -fdata-sections -Wl,--gc-sections -Wl,-z,norelro -Wl,--hash-style=gnu #-fno-exceptions
 
 	#strip+UPX to reduce the size of the binary without affecting correctness or performance
 	strip -S --strip-unneeded --remove-section=.note.gnu.gold-version --remove-section=.comment --remove-section=.note --remove-section=.note.gnu.build-id --remove-section=.note.ABI-tag "$AI_Name"
@@ -18,9 +19,13 @@ if [ -f "$AI_Name".cpp ]; then #If the file exists
 	Binary_To_Base85 "$AI_Name" "$AI_Name".cpp #Make the copy-pastable .cpp file containing the base85-encoded binary
     Base85_Filename="$AI_Name"_Base85.cpp
     Base85_Size=$(wc -c <"$Base85_Filename")
+    If0_Line=$(sed -n '/#if 0/=' $Base85_Filename)
+    let If0_NextLine=$If0_Line+2
+    Compiler_Version=$("$Compiler" --version | head -n 1)
+    echo "$If0_NextLine"' s/^/ Compiled with '"$Compiler_Version"'\n/'
+    sed -i "$If0_NextLine"' s/^/\/\/Compiled with '"$Compiler_Version"'\n/' $Base85_Filename
     if [ $Base85_Size -gt 100000 ]; then
     	echo File exceeded 100ko, not including source code as comment
-    	If0_Line=$(sed -n '/#if 0/=' $Base85_Filename)
     	sed -i -e "$If0_Line"',$d' $Base85_Filename
     fi
 
