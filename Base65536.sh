92b74196d92e4f25ea1fb41906a3059cc10d976b
Agade09 <n.sillitoe@gmail.com>
2020-12-03 22:24:18 +0000

    First version of base65536 exporter using python

--- Base65536.sh
+++ Base65536.sh
@@ -2,7 +2,7 @@ if [ $# -lt 1 ]; then
     echo "$0: Takes at least one argument, the name of the AI to compile"
     exit 1
 fi
-Compiler=g++-9
+Compiler=g++-10
 AI_Name=$1 #Get the AI name you passed as a command line parameter
 AI_Name="${AI_Name%.*}" #Remove extension in case you passed "V4.cpp" instead of "V4"
 if [ -f "$AI_Name".cpp ]; then #If the file exists
@@ -16,20 +16,20 @@ if [ -f "$AI_Name".cpp ]; then #If the file exists
 	#strip+UPX to reduce the size of the binary without affecting correctness or performance
 	strip -S --strip-unneeded --remove-section=.note.gnu.gold-version --remove-section=.comment --remove-section=.note --remove-section=.note.gnu.build-id --remove-section=.note.ABI-tag "$AI_Name"
 	upx "$AI_Name" --ultra-brute --no-backup
-	Binary_To_Base85 "$AI_Name" "$AI_Name".cpp #Make the copy-pastable .cpp file containing the base85-encoded binary
-    Base85_Filename="$AI_Name"_Base85.cpp
-    Base85_Size=$(wc -c <"$Base85_Filename")
-    If0_Line=$(sed -n '/#if 0/=' $Base85_Filename)
+	Binary_To_Base65536.py "$AI_Name" "$AI_Name".cpp #Make the copy-pastable .cpp file containing the base85-encoded binary
+    Base65536_Filename="$AI_Name"_Base85.cpp
+    #Base65536_Size=$(wc -c <"$Base65536_Filename")
+    If0_Line=$(sed -n '/#if 0/=' $Base65536_Filename)
     let If0_NextLine=$If0_Line+2
     if [[ $If0_Line -ne 0 ]]; then
         Compiler_Version=$("$Compiler" --version | head -n 1)
-        sed -i "$If0_NextLine"' s/^/\/\/Compiled with '"$Compiler_Version"'\n/' $Base85_Filename
+        sed -i "$If0_NextLine"' s/^/\/\/Compiled with '"$Compiler_Version"'\n/' $Base65536_Filename
     fi
 	
 	#Cleanup
 	rm "$AI_Name" #Remove the AI binary
 	rm "$AI_Name"_nopragma.cpp #Remove the copy of your code
-	xclip -selection c <  "$AI_Name"_Base85.cpp #Puts the copy-pastable .cpp file into your clipboard for quicker copy pasting
+	xclip -selection c <  "$AI_Name"_Base65536.py #Puts the copy-pastable .cpp file into your clipboard for quicker copy pasting
 else
 	echo "Could not find $AI_Name.cpp"
 fi
